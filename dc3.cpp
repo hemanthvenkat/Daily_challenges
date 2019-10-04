1d675b9691e8bfe99bf618e6986e4141ea631d4a
hemanthvenkat <54639601+hemanthvenkat@users.noreply.github.com>
2019-10-05 00:16:04 +0530

    Update and rename dc3.c to dc3.cpp

---
+++ dc3.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+#define ll long long
+using namespace std;
+
+
+int main(void) {
+	
+    //char s[100];
+	string s;
+    int l;
+	cin >> s;
+    //scanf("%s",s);
+    l = strlen(s);
+    int a = 26*(l+1)-l;
+	cout << a;
+    //printf("%d\n",a);
+    return 0;
+}
